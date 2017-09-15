6ea1c79472600de0ceca6c86f9b8109b79b3a9c3
cakedaychennai <twentyonefoods@gmail.com>
2017-09-16 01:34:40 +0530

    Changed datepicker to Pikaday to remove mostly unused JQuery-UI

--- js/order.js
+++ js/order.js
@@ -1,6 +1,11 @@
-$( function() {
-    $("#order-date").datepicker({
-        minDate: 1,
-        maxDate: 60
-    });
+var cakedayMinDate = new Date();
+cakedayMinDate.setDate(cakedayMinDate.getDate() + 1);
+var cakedayMaxDate = new Date();
+cakedayMaxDate.setDate(cakedayMinDate.getDate() + 60);
+
+var picker = new Pikaday({
+    field: document.getElementById("order-date"),
+    firstDay: 0,
+    minDate: cakedayMinDate,
+    maxDate: cakedayMaxDate
 });
